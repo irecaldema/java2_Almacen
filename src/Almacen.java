ec39795d4ac99736520dc10eec01e050498baecb
zubiri <irecaldema14dw@ikzubirimanteo.com>
2014-12-03 14:38:54 +0000

    3.12 primeras impresiones

--- src/Almacen.java
+++ src/Almacen.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.util.ArrayList;
+import java.util.scanner;
 public class Almacen {
 	public static void main (String args[]) throws IOException 	{
 		FileReader fr = new FileReader("distribuidores.txt");
@@ -10,39 +11,38 @@ public class Almacen {
 		System.out.println("\nLos distribuidores:");
 		String s;
 		ArrayList <Distribuidor> al_distri = new ArrayList <Distribuidor> ();		
-		while((s = br.readLine()) != null) 
-		{ 
+		while((s = br.readLine()) != null) { 
 			//creamos los objetos
 			Distribuidor distri=new Distribuidor();
 			Direccion dire=new Direccion();
 			Contacto conta=new Contacto();	
 			
-		    campos = s.split(",");
+			campos = s.split(",");
 		    
-		    //introducimos los valores en los objetos para despues añadirlos al ArrayList
-	 			//DISTRIBUIDOR
-	 			distri.setNombre(campos[0]);
-	 			distri.setCIF(campos[1]);
-	 			//DIRECCION
-	 			dire.setDireccion(campos[2]);	 			
-	 			/*
-	 			odireccion.setCiudad(separado[2]);
-	 			odireccion.setCalle(separado[3]);
-	 			odireccion.setNumero(Integer.parseInt(separado[4]));
-	 			odireccion.setCodPostal(Integer.parseInt(separado[5]));
-	 			*/
-	 			//CONTACTO
-	 			conta.setNombre(campos[3]);
-	 			conta.setApellido(campos[4]);
-	 			conta.setTelefono(Integer.parseInt(campos[5]));
-	 			//completamos los datos del distribuidor con los objetos
-				distri.setDireccion(dire);
-				distri.setPersonaContacto(conta);
-				//añadimos el objeto distribuidor al ArrayList
-				al_distri.add(distri);
+			 //introducimos los valores en los objetos para despues añadirlos al ArrayList
+	 		//DISTRIBUIDOR
+	 		distri.setNombre(campos[0]);
+	 		distri.setCIF(campos[1]);
+	 		//DIRECCION
+	 		dire.setDireccion(campos[2]);	 			
+	 		/*
+	 		odireccion.setCiudad(separado[2]);
+	 		odireccion.setCalle(separado[3]);
+	 		odireccion.setNumero(Integer.parseInt(separado[4]));
+	 		odireccion.setCodPostal(Integer.parseInt(separado[5]));
+	 		*/
+	 		//CONTACTO
+	 		conta.setNombre(campos[3]);
+	 		conta.setApellido(campos[4]);
+	 		conta.setTelefono(Integer.parseInt(campos[5]));
+	 		//completamos los datos del distribuidor con los objetos
+			distri.setDireccion(dire);
+			distri.setPersonaContacto(conta);
+			//añadimos el objeto distribuidor al ArrayList
+			al_distri.add(distri);
 		}
 
-	    for(int x=0; x<al_distri.size(); x++){
+		for(int x=0; x<al_distri.size(); x++){
 			System.out.println("--------------------------------");	
 			//nombre,CIF,o_direccion,o_personaContacto
 				System.out.println("nombre: " + al_distri.get(x).getNombre());
@@ -54,8 +54,53 @@ public class Almacen {
 					System.out.println("	" + al_distri.get(x).getPersonaContacto().getApellido());
 					System.out.println("	" + al_distri.get(x).getPersonaContacto().getTelefono());
 			System.out.println("--------------------------------");       
-        }   
+        	}   
+
+
+		Scanner sc = new Scanner(System.in);	
+		System.out.println("\nIntroduce la informacion de los productos");
+		
+		for (int m=0; m<2; m++)
+		{
+			System.out.println("\	manzana "+m+1+":");
+			/*
+			String tipoManzana, procedencia, color, 
+			Double eurosKilo;
+			Distribuidor distribuidor;
+    			*/
+    			Manzana manza = new Manzana();
+			//manza.setTipoManzana(sc.next());
+			manza.setDistribuidor(sc.next());
+		}
+		
+		System.out.println("\	lechuga:");
+		/*
+		String tipoLechuga, procedencia, color;
+		Double eurosUnidad;
+		Distribuidor distribuidor;
+    		*/
+    		Lechuga lechu = new Lechuga();
+		//lechu.setTipoLechuga(sc.next());
+		lechu.setDistribuidor(sc.next());
+		
+		for (int l=0; l<2; l++)
+		{
+			System.out.println("\n	leche"+l+1+":");
+			/*
+    			String tipo, procedencia;
+			Double eurosLitro;
+			Distribuidor distribuidor;
+    			*/
+    			Leche lec = new Leche();
+			//lec.setTipo(sc.next());
+			lec.setDistribuidor(sc.next())
+		}
+	
+			
+
+    
 		// Vaciar el ArrayList
-		al_distri.clear();
+		al_distri.clear();		
+		
 	}
 }	
